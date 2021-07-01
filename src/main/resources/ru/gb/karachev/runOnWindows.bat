98a3c4d8dfe1eb216fb44da2890dd1795a06b23b
Sasha Karachev <shuricans@gmail.com>
2021-07-01 20:54:17 +0300

    try jar package

---
+++ src/main/resources/ru/gb/karachev/runOnWindows.bat
@@ -0,0 +1,5 @@
+"C:\Users\sasha\.sdkman\candidates\java\11.0.7.j9-adpt\bin\java.exe" ^
+--module-path "C:\Users\sasha\.sdkman\candidates\java\javafx-sdk-11.0.2\lib" ^
+--add-modules=javafx.controls ^
+--add-modules=javafx.fxml ^
+-jar SimpleChat-1.0-SNAPSHOT.jar
